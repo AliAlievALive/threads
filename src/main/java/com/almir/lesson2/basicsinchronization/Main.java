946a9113c17ae6564db565e70fc15af7ccd983a9
almir <madarat87@gmail.com>
2023-02-01 09:09:16 +0300

    lesson2 - basic thread synchronization

---
+++ src/main/java/com/almir/lesson2/basicsinchronization/Main.java
@@ -0,0 +1,33 @@
+package com.almir.lesson2.basicsinchronization;
+
+import java.util.Scanner;
+
+class Processor extends Thread {
+    private volatile boolean running = true;
+    @Override
+    public void run() {
+        while (running) {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    public void shutdown() {
+        this.running = false;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Processor proc1 = new Processor();
+        proc1.start();
+
+        System.out.println("Press return to stop ...");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        proc1.shutdown();
+    }
+}
